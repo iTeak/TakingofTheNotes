41767dd30d0723b26db4c199b42b7864e27da434
Prateek Verghese <prateek@2Prateeks-MacBook-Pro.local>
2023-01-05 11:07:38 -0800

    adding routes and stuff

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,15 @@
 const { request } = require("express");
 const express = require ("express")
 const app = express()
-app.get ("/",(request,)=> {
-    res.send("Okay")
+app.get ("/",(req, res )=> {
+    res.send("Hello World")
 })
 app.listen(3000);
 console.log("running")
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', route1);
+app.use('/', routeshtml);
+const apiRoutes = require('./Routes/route1');
+const htmlRoutes = require('./Routes/routeshtml');
